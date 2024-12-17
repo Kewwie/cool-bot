6e1a0ea9b0054a314e045b4dffd84cd720c8e5e1
Kewi <iamkewwie@gmail.com>
2024-12-17 17:01:35 +0100

    update guild-module command

--- app/src/modules/config/commands/guild-module.ts
+++ app/src/modules/config/commands/guild-module.ts
@@ -1,42 +1,61 @@
-import { KiwiClient } from '@/client';
-import { SlashCommand } from '@/types/command';
+import { KiwiClient } from "@/client";
+import { SlashCommand } from "@/types/command";
 import {
 	Message,
 	EmbedBuilder,
 	User,
 	ChatInputCommandInteraction,
 	SlashCommandBuilder,
-} from 'discord.js';
+} from "discord.js";
 
 export const GuildModuleCommand: SlashCommand = {
 	config: new SlashCommandBuilder()
-		.setName('guild-module')
-		.setDescription('Manage the guilds modules')
+		.setName("guild-module")
+		.setDescription("Manage the guilds modules")
 		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('toggle')
-				.setDescription('Toggle a guild module')
+				.setName("toggle")
+				.setDescription("Toggle a guild module")
 				.addStringOption((option) =>
 					option
-						.setName('module')
+						.setName("module")
 						.setRequired(true)
-						.setDescription('The module to toggle')
-						.addChoices([{ name: 'Levels', value: 'levels' }])
+						.setDescription("The module to toggle")
+						.setAutocomplete(true)
 				)
 		)
 		.addSubcommand((subcommand) =>
-			subcommand.setName('view').setDescription('View the guilds modules')
+			subcommand.setName("view").setDescription("View the guilds modules")
 		),
 
-	async execute(
-		client: KiwiClient,
-		interaction: ChatInputCommandInteraction
-	): Promise<void> {
+	async autocomplete(client, interaction) {
+		var focused = interaction.options.getFocused(true);
+		var options;
+
+		switch (focused.name) {
+			case "module": {
+				let modules = client.ModuleManager.Modules.filter(
+					(module) => !module.default && !module.developerOnly
+				);
+				options = modules.map((module) => {
+					return {
+						name: module.id,
+						value: module.id,
+					};
+				});
+				break;
+			}
+		}
+
+		interaction.respond(options);
+	},
+
+	async execute(client: KiwiClient, interaction: ChatInputCommandInteraction): Promise<void> {
 		var cfg = await client.db.getGuildConfig(interaction.guild.id);
 		switch (interaction.options.getSubcommand(true)) {
-			case 'toggle': {
-				var module = interaction.options.getString('module');
-				var response = '';
+			case "toggle": {
+				var module = interaction.options.getString("module");
+				var response = "";
 				if (!cfg.modules[module]) {
 					cfg.modules[module] = true;
 					response = `**${client.capitalize(module)}:** Enabled`;
@@ -49,7 +68,7 @@ export const GuildModuleCommand: SlashCommand = {
 				break;
 			}
 
-			case 'view': {
+			case "view": {
 				var enabledModules = Object.keys(cfg.modules).filter(
 					(module) => cfg.modules[module] === true
 				);
@@ -59,9 +78,9 @@ export const GuildModuleCommand: SlashCommand = {
 
 				interaction.reply({
 					content: [
-						`**Enabled Modules**\n${enabledModules.join('\n')}`,
-						`**Disabled Modules**\n${disabledModules.join('\n')}`,
-					].join('\n\n'),
+						`**Enabled Modules**\n${enabledModules.join("\n")}`,
+						`**Disabled Modules**\n${disabledModules.join("\n")}`,
+					].join("\n\n"),
 				});
 				break;
 			}
