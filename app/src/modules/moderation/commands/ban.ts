2589c55c814ee1fa660f5796d3ca85bd050467d7
Kewi <iamkewwie@gmail.com>
2024-12-17 16:39:00 +0100

    added ban command

---
+++ app/src/modules/moderation/commands/ban.ts
@@ -0,0 +1,59 @@
+import { KiwiClient } from "@/client";
+import { CommandOptions, ConfigOptionTypes, PrefixCommand } from "@/types/command";
+import { Message, GuildMember, EmbedBuilder, PermissionFlagsBits } from "discord.js";
+
+export const BanPrefix: PrefixCommand = {
+	config: {
+		name: "ban",
+		description: "Ban a member",
+		aliases: ["banish", "terminate", "getout"],
+		autoDelete: false,
+		defaultPermissions: [PermissionFlagsBits.BanMembers],
+		options: [
+			{
+				name: "member",
+				type: ConfigOptionTypes.MEMBER,
+			},
+			{
+				name: "reason",
+				type: ConfigOptionTypes.TEXT,
+				includeAfter: true,
+				optional: true,
+				default: "No reason provided",
+			},
+		],
+	},
+
+	async execute(
+		client: KiwiClient,
+		message: Message,
+		commandOptions: CommandOptions,
+		member: GuildMember,
+		reason?: string
+	) {
+		if (message.author.bot) {
+			commandOptions.channel.send("You cannot timeout a bot");
+			return;
+		}
+
+		if (!member.bannable) {
+			commandOptions.channel.send("User cannot be banned");
+			return;
+		}
+
+		try {
+			await member.ban({ reason });
+			var timeoutEmbed = new EmbedBuilder()
+				.setTitle("User Banned")
+				.setColor("#2b2d31")
+				.addFields(
+					{ name: "User", value: `<@${member.id}>\n${member.user.username}` },
+					{ name: "Reason", value: reason }
+				)
+				.setFooter({ text: `Moderator: ${message.author.username}` });
+			commandOptions.channel.send({ embeds: [timeoutEmbed] });
+		} catch (error) {
+			commandOptions.channel.send("Could not ban user");
+		}
+	},
+};
