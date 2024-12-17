80727be0b7b3f9ed98e72c35efb7c182ce3ac45e
Kewi <iamkewwie@gmail.com>
2024-12-17 16:39:44 +0100

    Update module.ts

--- app/src/modules/moderation/module.ts
+++ app/src/modules/moderation/module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@/types/module";
 
 // Commands
+import { BanPrefix } from "./commands/ban";
+import { InfractionsPrefix } from "./commands/infractions";
 import { TimeoutPrefix } from "./commands/timeout";
+
+// Schedules
+import { CheckInfracitonsSchedule } from "./schedules/checkInfractions";
 
 export const ModerationModule: Module = {
 	id: "Moderation",
-	prefixCommands: [TimeoutPrefix],
+	prefixCommands: [BanPrefix, InfractionsPrefix, TimeoutPrefix],
+	schedules: [CheckInfracitonsSchedule],
 };
