f51557ac97c283060a6b5ff4ef077034a0bec07f
Kewi <iamkewwie@gmail.com>
2024-12-18 11:27:09 +0100

    added delete many command

---
+++ app/src/modules/moderation/commands/delete-many.ts
@@ -0,0 +1,59 @@
+import { KiwiClient } from "@/client";
+import { CommandOptions, ConfigOptionTypes, PrefixCommand } from "@/types/command";
+import { Message, GuildMember, EmbedBuilder, PermissionFlagsBits } from "discord.js";
+
+export const DeleteManyPrefix: PrefixCommand = {
+	config: {
+		name: "delete-many",
+		description: "Delete x amount of a users messages",
+		aliases: ["deletemany", "dm"],
+		autoDelete: true,
+		defaultPermissions: [PermissionFlagsBits.ManageMessages],
+		options: [
+			{
+				name: "member",
+				type: ConfigOptionTypes.MEMBER,
+			},
+			{
+				name: "amount",
+				type: ConfigOptionTypes.NUMBER,
+			},
+		],
+	},
+
+	async execute(
+		client: KiwiClient,
+		message: Message,
+		commandOptions: CommandOptions,
+		member: GuildMember,
+		amount: number
+	) {
+		amount = Math.min(amount, 50);
+		let fetchedMessages;
+		let userMessages = [];
+		let lastMessageId = message.id;
+		let maxSeachAmount = 1000;
+
+		while (userMessages.length < amount) {
+			fetchedMessages = await message.channel.messages.fetch({
+				limit: 50,
+				before: lastMessageId,
+			});
+			if (fetchedMessages.size === 0) break;
+			if ((maxSeachAmount -= 50) <= 0) break;
+
+			let filteredMessages = fetchedMessages.filter((msg) => msg.author.id === member.id);
+			userMessages = userMessages.concat(Array.from(filteredMessages.values()));
+
+			lastMessageId = fetchedMessages.last().id;
+		}
+
+		for (var messageToDelete of userMessages) {
+			try {
+				await messageToDelete.delete();
+			} catch (error) {
+				commandOptions.channel.send(`Could not delete message: ${messageToDelete.id}`);
+			}
+		}
+	},
+};
--- app/src/modules/moderation/module.ts
+++ app/src/modules/moderation/module.ts
@@ -2,6 +2,7 @@ import { Module } from "@/types/module";
 
 // Commands
 import { BanPrefix } from "./commands/ban";
+import { DeleteManyPrefix } from "./commands/delete-many";
 import { DeletePrefix } from "./commands/delete";
 import { InfractionsPrefix } from "./commands/infractions";
 import { TimeoutPrefix } from "./commands/timeout";
@@ -11,6 +12,6 @@ import { CheckInfracitonsSchedule } from "./schedules/checkInfractions";
 
 export const ModerationModule: Module = {
 	id: "moderation",
-	prefixCommands: [BanPrefix, DeletePrefix, InfractionsPrefix, TimeoutPrefix],
+	prefixCommands: [BanPrefix, DeleteManyPrefix, DeletePrefix, InfractionsPrefix, TimeoutPrefix],
 	schedules: [CheckInfracitonsSchedule],
 };
